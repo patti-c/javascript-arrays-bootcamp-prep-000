afb3758142c60f95079a02ffed2524ec150485c7
Patricia Carter <patricia4194@gmail.com>
2018-07-11 16:29:23 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -31,3 +31,8 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element);
   return array
 }
+
+function addElementToBeginningOfArray(array, element) {
+  var newArray = [element, ...array];
+  return newArray;
+}
