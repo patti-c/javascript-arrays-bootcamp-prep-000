f45b5da2bc736b17f66e493a2e8646ed2e858791
Patricia Carter <patricia4194@gmail.com>
2018-07-11 16:29:40 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -31,8 +31,3 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element);
   return array
 }
-
-function addElementToBeginningOfArray(array, element) {
-  var newArray = [element, ...array];
-  return newArray;
-}
